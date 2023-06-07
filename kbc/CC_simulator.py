dbe981ff200f625d9ca0de6a10c4ea9c1024469c
Armin Toroghi <49584324+atoroghi@users.noreply.github.com>
2023-06-06 20:46:22 -0400

    Update CC_simulator.py

--- kbc/CC_simulator.py
+++ kbc/CC_simulator.py
@@ -24,7 +24,7 @@ if __name__ == '__main__':
                 output_file = f"slurm_{cov_anchor}_{cov_var}_{cov_target}.out"
                 #command = ["python3", "-m", "kbc.cqd_beam_bpl", "data/Movielens", "--model_path", "models/Movielens-SimplE-model-rank-50-epoch-40-1684629098.pt", "--dataset", "Movielens", "--candidates", "3", "--quantifier", "marginal_ui", "--cov_target", str(cov_target), "--cov_var", str(cov_var), "--cov_anchor", str(cov_anchor)]
                 #command = ["python3", "-m", "kbc.cqd_beam_bpl", "data/Movielens", "--model_path", "models/Movielens-SimplE-model-rank-50-epoch-40-1684629098.pt", "--dataset", "Movielens", "--candidates", "10", "--quantifier", "marginal_i", "--cov_target", str(cov_target), "--cov_var", str(cov_var), "--cov_anchor", str(cov_anchor)]
-                command = ["python3", "-m", "kbc.cqd_beam_bpl", "data/LastFM", "--model_path", "models/LastFM-SimplE-model-rank-100-epoch-80-1686020763.pt", "--dataset", "LastFM", "--candidates", "10", "--quantifier", "marginal_ui", "--cov_target", str(cov_target), "--cov_var", str(cov_var), "--cov_anchor", str(cov_anchor)]
+                command = ["python3", "-m", "kbc.cqd_beam_bpl", "data/LastFM", "--model_path", "models/LastFM-SimplE-model-rank-100-epoch-80-1686020763.pt", "--dataset", "LastFM", "--candidates", "10", "--quantifier", "marginal_i", "--cov_target", str(cov_target), "--cov_var", str(cov_var), "--cov_anchor", str(cov_anchor)]
                 with open(output_file, "w") as f:
                     process = subprocess.Popen(command, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
                     stdout, stderr = process.communicate()
